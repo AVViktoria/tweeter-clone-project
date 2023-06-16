e0cc8c736d8c7d8ff6f8f5e7038e6d322fba7ad7
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-06-15 22:56:11 -0700

    feat: create SidebarItem, SidebarLog

--- tweeter-clone/components/layout/Sidebar.tsx
+++ tweeter-clone/components/layout/Sidebar.tsx
@@ -1,7 +1,46 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+
+import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
+
+
 const Sidebar = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notification",
+      href: "/notification",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
   return (
-    <div></div>
-  )
-}
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col items-end">
+          <div className="space-y-2 lg:w-[230px]">
+            <SidebarLogo />
+            {items.map((item) => (
+              <SidebarItem
+                key={item.href}
+                href={item.href} 
+                icon={item.icon} 
+                label={item.label}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
