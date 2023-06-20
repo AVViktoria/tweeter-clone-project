df1d4e68b533ebd1cb23e695e8ce7afb0cf5591f
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-06-19 23:37:11 -0700

    feat: create Avatar, added placeholder file to images

--- tweeter-clone/components/layout/FollowBar.tsx
+++ tweeter-clone/components/layout/FollowBar.tsx
@@ -1,19 +1,30 @@
-import { useRouter } from "next/router";
-
+import useUsers from "@/hooks/useUsers";
+import Avatar from "../Avatar";
+// import { useRouter } from "next/router";
 
 const FollowBar = () => {
-  const router = useRouter();
+  const { data: users = [] } = useUsers();
+  // const router = useRouter();
+  if (users.length === 0) {
+    return null;
+  }
   return (
-    <div
-      className="px-6 py-4 hidden lg:block"
-    >
+    <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-white text-xl font-semibold ">Who to follow</h2>
         <div className="flex flex-col gap-6 mt-4">
-           {/* todo users list */}
-          </div> 
+          {/* todo users list */}
+          {users.map((user: Record<string, any>) => (
+            <div key={user.id} className="flex flex-row gap-4">
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-white font-semibold text-sm">{user.name}</p>
+                <p className="text-neutral-400 text-sm">@{user.username}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      
     </div>
   );
 };
